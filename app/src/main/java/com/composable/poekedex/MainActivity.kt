7d1bed63bb729cd3f54ef7253496efa5c091371e
Omar Mohamed <98606902+OmarLkhalil@users.noreply.github.com>
2023-04-07 23:23:47 +0200

    SetupNavigation

--- app/src/main/java/com/composable/poekedex/MainActivity.kt
+++ app/src/main/java/com/composable/poekedex/MainActivity.kt
@@ -3,27 +3,56 @@ package com.composable.poekedex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.composable.poekedex.ui.theme.JetpackComposePokedexTheme
 
+/**
+ * The main activity of the Pokedex app. Extends ComponentActivity, which is a subclass of
+ * Activity that adds support for lifecycle and other architectural components.
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Set the content of the activity using Jetpack Compose.
         setContent {
+            // Apply a theme to the entire app.
             JetpackComposePokedexTheme {
+                // Create a NavController, which will handle navigation between screens.
                 val navController = rememberNavController()
+                // Create a NavHost, which will contain the app's screens.
                 NavHost(navController = navController, startDestination = "pokemon_list_screen"){
+                    // Add a composable for the "pokemon_list_screen".
+                    composable("pokemon_list_screen"){
+                        // TODO: Add content for the "pokemon_list_screen".
+                    }
+                    // Add a composable for the "pokemon_detail_screen".
+                    composable("pokemon_detail_screen", arguments = listOf(
+                        // Add two arguments to the "pokemon_detail_screen".
+                        navArgument("dominantColor"){
+                            type = NavType.IntType
+                        },
+                        navArgument("pokemonName"){
+                            type = NavType.StringType
+                        }
+                    ))
+                    {
+                        // Retrieve the values of the arguments from the NavBackStackEntry.
+                        val dominantColor = remember {
+                            it.arguments?.getInt("dominantColor")
+                        }
+                        val pokemonName = remember {
+                            it.arguments?.getString("pokemonName")
+                        }
+                        // TODO: Display the details of the selected Pokemon using the retrieved arguments.
+                    }
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
---
+++ app/src/main/java/com/composable/poekedex/PokeApplication.kt
@@ -0,0 +1,24 @@
+package com.composable.poekedex
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+/**
+ * An application class that is annotated with HiltAndroidApp, which sets up Hilt for dependency
+ * injection. Hilt is a dependency injection library built on top of Dagger.
+ */
+@HiltAndroidApp
+class PokeApplication : Application() {
+
+    /**
+     * Called when the application is starting, before any other application objects have been
+     * created. This method is used to initialize the application and set up any necessary
+     * components.
+     */
+    override fun onCreate() {
+        super.onCreate()
+        // Plant a new instance of Timber's DebugTree, which is used for logging during development.
+        Timber.plant(Timber.DebugTree())
+    }
+}
